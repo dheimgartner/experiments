3003e21fdf29fb5c5d09737a7e3bd630301903dd
dheimgartner <d.heimgartners@gmail.com>
2022-12-22 14:12:45 +0100

    removed variable cost (we only need per km cost)

--- data-raw/generic_archetypes.R
+++ data-raw/generic_archetypes.R
@@ -4,6 +4,8 @@ devtools::load_all()
 
 activate_venv()
 
-generic_archetypes <- experiments::gen_archs(experiments::cars$df)
+generic_archetypes <-
+  experiments::gen_archs(experiments::cars$df) %>%
+  select(-variable_cost)
 
 usethis::use_data(generic_archetypes, overwrite = TRUE)
