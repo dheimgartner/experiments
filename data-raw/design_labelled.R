322bd5178ff4482fcb49f515dcb00dc9a8ae6657
dheimgartner <d.heimgartners@gmail.com>
2022-12-22 14:13:09 +0100

    will be what we server from quapi

---
+++ data-raw/design_labelled.R
@@ -0,0 +1,32 @@
+## code to prepare `design_labelled` dataset goes here
+
+library(tidyverse)
+library(jsonlite)
+
+devtools::load_all()
+
+design_labelled <- list()
+
+df <-
+  experiments::replace_effect_codes(experiments::design$final,
+                                    add_units = TRUE)
+
+df <-
+  df %>%
+  group_by(block) %>%
+  mutate(id = dplyr::row_number()) %>%
+  ungroup() %>%
+  select(block, id, everything())
+
+df <-
+  df %>%
+  select(-contains("_available"))
+
+json <- jsonlite::toJSON(df, na = "string", pretty = TRUE)
+
+jsonlite::write_json(json, "./data/design_labelled.json")
+
+design_labelled$df <- df
+design_labelled$json <- json
+
+usethis::use_data(design_labelled, overwrite = TRUE)
